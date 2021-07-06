12e39fd941d6d9636644a8706e7d6f7d720b79db
Carlos Sol <fersolmarin@gmail.com>
2021-07-05 21:59:10 -0500

    Sidebar Fixed

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,35 +2,31 @@
     <div class="row">
         <div class="side-nav">
             <div class="nav-logo">
-                <a href="/">
-                    <img src="{{ asset('images/logo.png') }}" alt="Logo" />
-                </a>
+                <img src="{{ asset('images/logo.png') }}" alt="Logo" />
             </div>
 
             <div class="nav-actions">
                 <div>
-                <a href="#"><span class="icon"><i class="fa fa-plus" aria-hidden="true"></i></span>
+                    <a href="/admin/project/create"><span class="icon"><i class="fa fa-plus" aria-hidden="true"></i></span>
 
                    <span class="hidden">Crear projecto</span>
                 </a>
                 </div>
                 
                 <div>
-                    <a href="/"> <span class="icon"><i class="fa fa-list" aria-hidden="true"></i></span>
+                    <a href="/admin/project"> <span class="icon"><i class="fa fa-list" aria-hidden="true"></i></span>
 
                    <span class="hidden">Proyectos en curso</span>
                 </a>
                 </div>
 
                 <div>
-                <a href="/users"><span class="icon"><i class="fa fa-users" aria-hidden="true"></i></span>
+                    <a href="/admin/user"><span class="icon"><i class="fa fa-users" aria-hidden="true"></i></span>
 
                     <span class="hidden">Base de datos</span>
                 </a>
                 </div>
             </div>
         </div>
-
-        <div class="content">@yield('content')</div>
     </div>
 </div>
